d9fc784260281c6bfcfe6cefb4ac4b6759ba24b2
Andrew Christabel <andrew.christabel@myob.com>
2022-06-06 15:23:23 +1000

    Moved calculation of coordinate distance to CoordinateExtensions.cs

--- Pacman/Business/Control/CoordinateExtensions.cs
+++ Pacman/Business/Control/CoordinateExtensions.cs
@@ -9,4 +9,11 @@ public static class CoordinateExtensions
         var (x, y) = coordinate;
         return x + y * width;
     }
+
+    public static double GetDistance(this Coordinate coord1, Coordinate coord2)
+    {
+        var (x1, y1) = coord1;
+        var (x2, y2) = coord2;
+        return Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+    }
 }
