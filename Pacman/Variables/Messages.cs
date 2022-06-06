6d9f2db519d9390abdd3bb18859500c889b34252
Andrew Christabel <andrew.christabel@myob.com>
2022-06-06 15:30:02 +1000

    Added message for completing a round

--- Pacman/Variables/Messages.cs
+++ Pacman/Variables/Messages.cs
@@ -5,15 +5,16 @@ namespace Pacman.Variables;
 
 public static class Messages
 {
-    public const string InvalidKeyPress = "Invalid key press. Use arrow keys to move: ";
-    public const string WallObstruction = "Pacman cannot move into a wall. Please try again: ";
+    public const string InvalidKeyPress = "Invalid key press. Use arrow keys to move:\n";
+    public const string WallObstruction = "Pacman cannot move into a wall. Please try again:\n";
     public const string GhostCollision = "You collided with a ghost! Press any key to continue...";
+    public const string RoundComplete = "Round complete! Press any key to continue...";
 
     public static string GetTurnInfo(GameState gameState)
     {
         var res = new StringBuilder();
         res.Append($"Lives: {new string(Constants.Heart, gameState.Pac.Lives)}\n");
-        res.Append($"Round: {gameState.Round}/{Constants.MaxRounds}\n");
+        res.Append($"Round: {Math.Min(gameState.Round, Constants.MaxRounds)}/{Constants.MaxRounds}\n");
         res.Append($"Pellets remaining: {gameState.Pellets.Count()}\n");
 
         return res.ToString();
@@ -21,7 +22,7 @@ public static class Messages
     
     public static string GetGameOutcome(GameState gameState)
     {
-        return gameState.Round == Constants.MaxRounds ? 
+        return gameState.Round > Constants.MaxRounds ? 
             "\nCongratulations! You win!\n" : 
             "\nYou lost! Better luck next time!\n";
     }
